59811b0952c61d36c06e7b7374dd1fb6dcdf5733
Jaymie Batoon <jaymiebatoon@Jaymies-MacBook-Pro-2.local>
2022-06-25 01:24:25 -0400

    Populated database and updated serilializer to display id.

--- supers/serializers.py
+++ supers/serializers.py
@@ -5,5 +5,5 @@ from .models import Supers
 class SupersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supers
-        fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catch_phrase']
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catch_phrase']
         
